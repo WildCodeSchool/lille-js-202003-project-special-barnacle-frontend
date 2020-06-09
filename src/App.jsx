32e8dfa1e6f87185a2f97a539f33cbb19e9008a5
berenice89 <berenice.despres@gmail.com>
2020-06-09 17:57:00 +0200

    added txt and img to the footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,20 @@
 import React from 'react';
 import './App.css';
 import Header from './components/Header';
+import Footer from './components/Footer';
 
 function App() {
   return (
-    <div className="App">
+    <div>
       <Header />
+      <p>_________________</p>
+      <p>_________________</p>
+      <p>_________________</p>
+      <p>HOMEPAGE</p>
+      <p>_________________</p>
+      <p>_________________</p>
+      <p>_________________</p>
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,105 @@
+import React from 'react';
+import './styles/footer.css';
+
+const Footer = () => {
+  return (
+    <div>
+      <div className="footer">
+        <div>
+          <img
+            src="./img/logofooter.png"
+            alt="logo Footer Negodi"
+            width="150px"
+          />
+        </div>
+        <div className="footerlists">
+          <div className="footerlist">
+            <h3>à propos de negodi</h3>
+            <ul>
+              <li>Découvrez Negodi</li>
+              <li>Un conseil indépendant ?</li>
+              <li>Frais de courtage et rétrocession de commision</li>
+              <li>Mentions légales</li>
+            </ul>
+          </div>
+          <div className="footerlist">
+            <h3>nos services</h3>
+            <ul>
+              <li>Crédit immobilier</li>
+              <li>Assurance emprunteur</li>
+              <li>Recommandez ce site</li>
+              <li>Espace clients</li>
+            </ul>
+          </div>
+          <div className="footerlist">
+            <ul>
+              <li>02 41 01 23 45</li>
+
+              <li>
+                <img src="./img/mail.png" alt="mail" width="50px" />
+                <span id="mail">contact@negodi.fr</span>
+              </li>
+              <li>Consultez notre FAQ</li>
+              <li>Envoyez nous un e-mail</li>
+            </ul>
+            <h3>Suivez-nous</h3>
+            <div className="social">
+              <img
+                src="./img/iconfacebook.png"
+                alt="facebook"
+                height="80px"
+                width="80px"
+              />
+              <img
+                src="./img/icontwitter.png"
+                alt="facebook"
+                height="80px"
+                width="80px"
+              />
+              <img
+                src="./img/iconlinkedin.png"
+                alt="facebook"
+                height="80px"
+                width="80px"
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="footerbottom">
+        <p>
+          ASC Agence de Services en Courtages - RCS Angers 792 071 425 00013
+        </p>
+        <p>
+          EURL au capital social de 10 000€ - Siège Social: 2, Quare La Fayette
+          - 49000 Angers - Tél:06.64.69.08.12 - Courriel: asc.elattar@gmail.com
+        </p>
+        <p>
+          Conseil en Investissements Financiers référencé sous le N°F000367 par
+          la Compagnie des CGPI, Association agréée par l Autorité des Marchés
+          Financiers
+        </p>
+        <p>
+          Courtier en Assurance et en Opérations Bancaires et Services de
+          Paiement inscrti à l ORIAS sous le N°13 004 721
+        </p>
+        <p>
+          Carte professionnelle Immobilière n°2013 49 410 T délivrée par la
+          préfecture du département 49
+        </p>
+        <p>
+          Assurance de Responsabilité Civile Professionnelle conformes aux
+          articles L 530-1 et L 530-2 du Code des Assurances souscrites auprès
+          de la Société d Assurances CGPA
+        </p>
+        <p>
+          125 Rue de la Faisanderie - 75773 Paris Cedex 16 - Police n RCPGP3215
+        </p>
+        <div />
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
