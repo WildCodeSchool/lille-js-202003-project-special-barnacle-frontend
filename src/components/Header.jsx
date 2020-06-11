b7198b0d394fb559d03353a2a3d52d0555935a25
Julien Martos <ju_sxm@hotmail.fr>
2020-06-11 22:00:03 +0200

    Add large screen class with bootstrap for header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -23,37 +23,36 @@ const Header = () => {
     <StyledHeader open={open}>
       <div className="container-fluid px-0">
         <div className="row mx-auto ">
-          <div className="col-2 col-sm-4 mt-3 ">
+          <div className="col-2 col-sm-4 mt-3 d-lg-none">
             <Burger open={open} setOpen={setOpen} />
           </div>
           <Menu open={open} setOpen={setOpen} />
-          <Link to="/" className="col-7 col-sm-5 col-lg-1">
+          <Link to="/" className="col-7 col-sm-5 col-lg-3">
             <img
               className="logo-box mt-2"
               src="./img/logoNegodi.png"
               alt="logo Negodi"
             />
           </Link>
-          <div className=" offset-lg-3" />
-          <nav className="d-none d-lg-block col-lg-6  my-auto">
-            <NavLink className="mx-3  navLink" to="/about">
+          <nav className="d-none d-lg-block col-lg-8 my-auto">
+            <NavLink className="mx-2  navLink" to="/about">
               Découvrez Negodi
             </NavLink>
-            <NavLink className="mx-3" to="/offer">
+            <NavLink className="mx-2" to="/offer">
               Nos offres
             </NavLink>
-            <NavLink className="mx-3" to="/project">
+            <NavLink className="mx-2" to="/project">
               Démarrez votre projet
             </NavLink>
-            <NavLink className="mx-3" to="/contact">
+            <NavLink className="mx-2" to="/contact">
               Contact
             </NavLink>
-            <NavLink className="mx-3" to="/user">
+            <NavLink className="mx-2" to="/user">
               Espace Client
             </NavLink>
           </nav>
-          <div className="offset-1 offset-lg-1" />
-          <Link href="/" className="col-2 col-sm-2 mt-2">
+          <div className="offset-1 offset-lg-0" />
+          <Link href="/" className="col-2 col-sm-2 col-lg-1 mt-2">
             <img
               className="login"
               src="./img/user.png"
