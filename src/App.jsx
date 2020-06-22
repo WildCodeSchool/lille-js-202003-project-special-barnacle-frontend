b2bea60ec50daa4574b5090521634e06a19e220d
Julien Martos <ju_sxm@hotmail.fr>
2020-06-22 17:10:28 +0200

    delete header.css and styles folder. Order import by DSC

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
-import './App.css';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Header from './components/Header';
 import Homepage from './components/Homepage';
+import './App.css';
 
 function App() {
   return (
