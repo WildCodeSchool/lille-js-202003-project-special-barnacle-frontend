3eb61a57c6c13aadd1e155c640f9a917eaf521ff
Julien Martos <ju_sxm@hotmail.fr>
2020-06-22 16:52:53 +0200

    delete dead code, header.css file and folder styles. Order import by DSC

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
-import './App.css';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Header from './components/Header';
+import './App.css';
 
 function App() {
   return (
--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 import styled from 'styled-components';
 import PropTypes from 'prop-types';
-import { NavLink } from 'react-router-dom';
 
 const StyledMenu = styled.nav`
   display: flex;
