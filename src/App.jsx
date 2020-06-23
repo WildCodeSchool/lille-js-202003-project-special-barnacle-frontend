011b6e36da99a4f3ee07f7e72edf02693aa7cfbe
berenice89 <berenice.despres@gmail.com>
2020-06-23 14:54:26 +0200

    created form steps component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Footer from './components/Footer';
+import FormSteps from './components/form/FormSteps';
 import './App.css';
 
 function App() {
   return (
     <div>
+      <FormSteps />
       <Footer />
     </div>
   );
---
+++ src/components/form/FormSteps.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function FormSteps() {
+  return <div>FormSteps</div>;
+}
+
+export default FormSteps;
