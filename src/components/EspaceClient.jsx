18f6cb403312acd1ce30a85b1e73a9dd6d19d9f2
Julien Martos <ju_sxm@hotmail.fr>
2020-06-23 10:00:00 +0200

    install boostrap and put flex on title

--- src/components/EspaceClient.jsx
+++ src/components/EspaceClient.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import LockIcon from '@material-ui/icons/Lock';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const EspaceClient = () => {
   return (
     <div className="container-fluid">
-      <div className="row">
-        <LockIcon fontSize="large" />
-        <h1>ACCES A VOTRE ESPACE CLIENT</h1>
+      <div className="row d-flex justify-content-center align-items-center">
+        <LockIcon fontSize="large" className="col-2 " />
+        <h1 className="col-8 text-center">ACCES A VOTRE ESPACE CLIENT</h1>
+        <div className="offset-2" />
       </div>
     </div>
   );
