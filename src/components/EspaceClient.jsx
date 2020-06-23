a09987b2d0884b39cb442515317958f243eed954
Julien Martos <ju_sxm@hotmail.fr>
2020-06-23 17:43:51 +0200

    Add image accross form in destock mode only. Still have to put it on right side

--- src/components/EspaceClient.jsx
+++ src/components/EspaceClient.jsx
@@ -12,6 +12,12 @@ import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+// TODO mettre le caméléon avec un left à 70% pour le positionné à droite du formulaire grace au position-absolute
+// const Cameleon = styled.h1`
+//   margin-left: 50px;
+// `;
 
 function Copyright() {
   return (
@@ -49,68 +55,77 @@ export default function SignIn() {
   const classes = useStyles();
 
   return (
-    <Container component="main" maxWidth="xs">
-      <CssBaseline />
-      <div className={classes.paper}>
-        <Avatar className={classes.avatar}>
-          <LockOutlinedIcon />
-        </Avatar>
-        <Typography component="h1" variant="h5">
-          Accès à votre espace client
-        </Typography>
-        <form className={classes.form} noValidate>
-          <TextField
-            variant="outlined"
-            margin="normal"
-            required
-            fullWidth
-            id="email"
-            label="Adresse Email"
-            name="email"
-            autoComplete="email"
-            autoFocus
-          />
-          <TextField
-            variant="outlined"
-            margin="normal"
-            required
-            fullWidth
-            name="password"
-            label="Mot de passe"
-            type="password"
-            id="password"
-            autoComplete="current-password"
-          />
-          <FormControlLabel
-            control={<Checkbox value="remember" color="primary" />}
-            label="Se rappeler de moi"
-          />
-          <Button
-            type="submit"
-            fullWidth
-            variant="contained"
-            color="primary"
-            className={classes.submit}
-          >
-            Connexion
-          </Button>
-          <Grid container>
-            <Grid item xs>
-              <Link href="http://negodi.fr" variant="body2">
-                Mot de passe oublié?
-              </Link>
+    <div className="d-flex m-auto align-items-center">
+      <Container component="main" maxWidth="xs">
+        <CssBaseline />
+        <div className={classes.paper}>
+          <Avatar className={classes.avatar}>
+            <LockOutlinedIcon />
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            Accès à votre espace client
+          </Typography>
+
+          <form className={classes.form} noValidate>
+            <TextField
+              variant="outlined"
+              margin="normal"
+              required
+              fullWidth
+              id="email"
+              label="Adresse Email"
+              name="email"
+              autoComplete="email"
+              autoFocus
+            />
+            <TextField
+              variant="outlined"
+              margin="normal"
+              required
+              fullWidth
+              name="password"
+              label="Mot de passe"
+              type="password"
+              id="password"
+              autoComplete="current-password"
+            />
+            <FormControlLabel
+              control={<Checkbox value="remember" color="primary" />}
+              label="Se rappeler de moi"
+            />
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              color="primary"
+              className={classes.submit}
+            >
+              Connexion
+            </Button>
+            <Grid container>
+              <Grid item xs>
+                <Link href="http://negodi.fr" variant="body2">
+                  Mot de passe oublié?
+                </Link>
+              </Grid>
+              <Grid item>
+                <Link href="http://negodi.fr" variant="body2">
+                  Pas de compte? S&apos;inscrire
+                </Link>
+              </Grid>
             </Grid>
-            <Grid item>
-              <Link href="http://negodi.fr" variant="body2">
-                Pas de compte? S&apos;inscrire
-              </Link>
-            </Grid>
-          </Grid>
-        </form>
-      </div>
-      <Box mt={8}>
-        <Copyright />
-      </Box>
-    </Container>
+          </form>
+          <Box mt={8}>
+            <Copyright />
+          </Box>
+        </div>
+      </Container>
+      <img
+        src="../1_login-removebg-preview.png"
+        alt="login"
+        className="d-none d-lg-block align-items-center position-absolute"
+        style={{ height: 300 }}
+      />
+    </div>
   );
 }
