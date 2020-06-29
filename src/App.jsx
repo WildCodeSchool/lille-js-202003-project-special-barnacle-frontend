769b1a9a9fb7d3f178821a2c698199572944bbba
yann <yanndubois4@gmail.com>
2020-06-29 11:23:09 +0200

    Redux settings

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import Footer from './components/Footer';
+import CounterContainers from './components/containers/CounterContainers';
 import './App.css';
 
 function App() {
   return (
     <div>
       <Footer />
+      <CounterContainers />
     </div>
   );
 }
