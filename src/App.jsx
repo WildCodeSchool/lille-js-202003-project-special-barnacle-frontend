c0115bad11f74d508be0b857ccfd16c7ed23161b
berenice89 <berenice.despres@gmail.com>
2020-06-30 11:14:31 +0200

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
 import React from 'react';
 import Footer from './components/Footer';
-import FormSteps from './components/form/FormSteps';
+
 import './App.css';
 
 function App() {
   return (
     <div>
-      <FormSteps />
       <Footer />
     </div>
   );
--- src/components/form/FormSteps.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-function FormSteps() {
-  return <div>FormSteps</div>;
-}
-
-export default FormSteps;
