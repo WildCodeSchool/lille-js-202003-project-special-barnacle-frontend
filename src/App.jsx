23bc5c7f14d47c8878c890ea4ef267f24f83be77
berenice89 <berenice.despres@gmail.com>
2020-06-30 11:44:49 +0200

    added about page and changed the HP page in the router

--- src/App.jsx
+++ src/App.jsx
@@ -12,14 +12,18 @@ function App() {
       <div className="App">
         <Header />
         <Switch>
-          <Route path="/about" component={() => <div>about</div>} />
+          <Route exact path="/">
+            <Homepage />
+          </Route>
+          <Route path="/about">
+            <Apropos />
+          </Route>
           <Route path="/offer" component={() => <div>offer</div>} />
-          <Route exact path="/" component={() => <div />} />
           <Route path="/project" component={() => <div>Votre projet</div>} />
           <Route path="/contact" component={() => <div>contact</div>} />
           <Route path="/user/:id" component={() => <div>user</div>} />
         </Switch>
-        <Homepage />
+
         <Footer />
       </div>
     </Router>
