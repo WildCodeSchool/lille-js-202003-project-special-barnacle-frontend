a35f807be8b432cdb8c1c2b1bade07d16583046d
berenice89 <berenice.despres@gmail.com>
2020-07-02 16:43:27 +0200

    stepper implemented with material ui

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import Footer from './components/Footer';
+import Stepper from './components/Stepper';
 
 import './App.css';
 
 function App() {
   return (
     <div>
+      <Stepper />
       <Footer />
     </div>
   );
