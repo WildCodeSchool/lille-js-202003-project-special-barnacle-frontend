5ae06604bdd0eac535b1e93f2a6c7121d044ba68
berenice89 <berenice.despres@gmail.com>
2020-07-03 08:45:15 +0200

    add input type img to change the state and going to step 2

--- src/components/form/Form.jsx
+++ src/components/form/Form.jsx
@@ -1,71 +1,68 @@
-// import React, { useState } from 'react';
-// import Question1 from './projet/Question1';
-// import Question2 from './projet/Question2';
+import React, { useState } from 'react';
+import Question1 from './projet/Question1';
+import Question2 from './projet/Question2';
 
-// const Form = () => {
-//   const [data, setData] = useState({
-//     step: 1,
-//     question1: '',
-//     question2: '',
-//   });
+const Form = () => {
+  const [data, setData] = useState({
+    step: 1,
+    question1: '',
+    question2: '',
+  });
 
-//   // Proceed to next step
-//   const nextStep = () => {
-//     setData({
-//       ...data,
-//       step: step + 1,
-//     });
-//   };
+  // Proceed to next step
+  const nextStep = () => {
+    setData({
+      ...data,
+      step: +1,
+    });
+  };
 
-//   // Go back to prev step
-//   const prevStep = () => {
-//     setData({
-//       ...data,
-//       step: step - 1,
-//     });
-//   };
+  // Go back to prev step
+  const prevStep = () => {
+    setData({
+      ...data,
+      step: -1,
+    });
+  };
 
-//   // Handle fields change
-//   const handleChange = (e, input) => {
-//     console.log(e.target);
-//     setData({ ...data, [input]: e.target.value });
-//     // setData({ ...data, step: step + 1, [input]: e.target.value });
-//   };
+  // Handle fields change
+  const handleChange = (e, input) => {
+    setData({ ...data, step: +1, [input]: e.target.value });
+  };
 
-//   const { step } = data;
-//   const { firstName, lastName, email, occupation, city, bio } = data;
-//   const values = { firstName, lastName, email, occupation, city, bio };
+  const { step } = data;
+  const { firstName, lastName, email, occupation, city, bio } = data;
+  const values = { firstName, lastName, email, occupation, city, bio };
 
-//   switch (step) {
-//     case 1:
-//       return (
-//         <div>
-//           <Question1
-//             nextStep={nextStep}
-//             handleChange={handleChange}
-//             values={values}
-//           />
-//         </div>
-//       );
-//     case 2:
-//       return (
-//         <div>
-//           <Question2
-//             nextStep={nextStep}
-//             prevStep={prevStep}
-//             handleChange={handleChange}
-//             values={values}
-//           />
-//         </div>
-//       );
-//     case 3:
-//       return null;
-//     case 4:
-//       return null;
-//     default:
-//       console.log('This is a multi-step form built with React.');
-//       return null;
-//   }
-// };
+  switch (step) {
+    case 1:
+      return (
+        <div>
+          <Question1
+            nextStep={nextStep}
+            handleChange={handleChange}
+            values={values}
+          />
+        </div>
+      );
+    case 2:
+      return (
+        <div>
+          <Question2
+            nextStep={nextStep}
+            prevStep={prevStep}
+            handleChange={handleChange}
+            values={values}
+          />
+        </div>
+      );
+    case 3:
+      return null;
+    case 4:
+      return null;
+    default:
+      return null;
+  }
+};
 
-// export default Form;
+export default Form;
--- src/components/form/projet/Question1.jsx
+++ src/components/form/projet/Question1.jsx
@@ -1,108 +1,105 @@
-// import React from 'react';
-// import styled from 'styled-components';
+import React from 'react';
+import PropTypes from 'prop-types';
+import styled from 'styled-components';
 
-// const StyledForm = styled.section`
-//   h1 {
-//     color: #2a96af;
-//   }
+const StyledForm = styled.section`
+  h1 {
+    color: #2a96af;
+  }
 
-//   .form {
-//     background-color: #edf1f5;
-//   }
+  .form {
+    background-color: #edf1f5;
+  }
 
-//   .formCard {
-//     border-radius: 10px;
-//     -webkit-box-shadow: 0px 0px 70px -26px rgba(0, 0, 0, 0.54);
-//     -moz-box-shadow: 0px 0px 70px -26px rgba(0, 0, 0, 0.54);
-//     box-shadow: 0px 0px 70px -26px rgba(0, 0, 0, 0.54);
-//     transition: all 300ms;
-//     background-color: #173753;
-//   }
+  .formCard {
+    border-radius: 10px;
+    -webkit-box-shadow: 0px 0px 70px -26px rgba(0, 0, 0, 0.54);
+    -moz-box-shadow: 0px 0px 70px -26px rgba(0, 0, 0, 0.54);
+    box-shadow: 0px 0px 70px -26px rgba(0, 0, 0, 0.54);
+    transition: all 300ms;
+    background-color: #173753;
+  }
 
-//   .formCard:hover {
-//     transform: translate(0%, -1%);
-//   }
+  .formCard:hover {
+    transform: translate(0%, -1%);
+  }
 
-//   img {
-//     border-radius: 10px 10px 0 0;
-//   }
+  img {
+    border-radius: 10px 10px 0 0;
+  }
 
-//   p {
-//     color: #fff;
-//     font-weight: 700;
-//   }
-// `;
+  p {
+    color: #fff;
+    font-weight: 700;
+  }
+`;
 
-// function Question1({ values, handleChange, nextStep }) {
-//   return (
-//     <StyledForm>
-//       <form
-//         onClick={(e) => {
-//           e.preventDefault();
-//         }}
-//       >
-//         <div className="form container-fluid pb-5">
-//           <div className="row-cols-12 p-5 text-center">
-//             <h1>Quel est votre projet ?</h1>
-//           </div>
-//           <div className="row text-center">
-//             <div className="col-10 col-sm-5 col-lg-2 px-0 formCard mb-5 text-center mx-auto">
-//               <button
-//                 type="button"
-//                 value="goooo"
-//                 onClick={(e) => {
-//                   console.log('tututut');
-//                   handleChange(e, 'question1');
-//                 }}
-//               >
-//                 <img
-//                   className="col-12 p-0"
-//                   src="img/form/question1/acheterunbien.png"
-//                   alt="acheter un bien"
-//                   value="goooo"
-//                 />
-//               </button>
+function Question1({ handleChange }) {
+  return (
+    <StyledForm>
+      <form>
+        <div className="form container-fluid pb-5">
+          <div className="row-cols-12 p-5 text-center">
+            <h1>Quel est votre projet ?</h1>
+          </div>
+          <div className="row text-center">
+            <div className="col-10 col-sm-5 col-lg-2 px-0 formCard mb-5 text-center mx-auto">
+              <input
+                id="image"
+                value="goooo"
+                type="image"
+                alt="Login"
+                src="img/form/question1/acheterunbien.png"
+                width="100%"
+                onClick={(e) => {
+                  handleChange(e, 'question1');
+                }}
+              />
 
-//               <p className="my-auto py-2">Acheter un bien</p>
-//             </div>
+              <p className="my-auto py-2">Acheter un bien</p>
+            </div>
 
-//             <div className="col-10 col-sm-5 col-lg-2 px-0 formCard mb-5 text-center mx-auto">
-//               <img
-//                 className="col-12 p-0"
-//                 src="img/form/question1/faireconstruire.png"
-//                 alt="faire construire"
-//               />
-//               <p className="my-auto py-2">Faire construire</p>
-//             </div>
-//             <div className="col-10 col-sm-5 col-lg-2 px-0 formCard mb-5 text-center mx-auto">
-//               <img
-//                 className="col-12 p-0"
-//                 src="img/form/question1/fairedestravaux.png"
-//                 alt="faire des travaux"
-//               />
-//               <p className="my-auto py-2">Faire des travaux</p>
-//             </div>
-//             <div className="col-10 col-sm-5 col-lg-2 px-0 formCard mb-5 text-center mx-auto">
-//               <img
-//                 className="col-12 p-0"
-//                 src="img/form/question1/renegocier.png"
-//                 alt="renegocier"
-//               />
-//               <p className="my-auto py-2">Renégocier</p>
-//             </div>
-//             <div className="col-10 col-sm-5 col-lg-2 px-0 formCard mb-5 text-center mx-auto">
-//               <img
-//                 className="col-12 p-0"
-//                 src="img/form/question1/autresprojets.jpg"
-//                 alt="autres projets"
-//               />
-//               <p className="my-auto py-2">Autres projets</p>
-//             </div>
-//           </div>
-//         </div>
-//       </form>
-//     </StyledForm>
-//   );
-// }
+            <div className="col-10 col-sm-5 col-lg-2 px-0 formCard mb-5 text-center mx-auto">
+              <img
+                className="col-12 p-0"
+                src="img/form/question1/faireconstruire.png"
+                alt="faire construire"
+              />
+              <p className="my-auto py-2">Faire construire</p>
+            </div>
+            <div className="col-10 col-sm-5 col-lg-2 px-0 formCard mb-5 text-center mx-auto">
+              <img
+                className="col-12 p-0"
+                src="img/form/question1/fairedestravaux.png"
+                alt="faire des travaux"
+              />
+              <p className="my-auto py-2">Faire des travaux</p>
+            </div>
+            <div className="col-10 col-sm-5 col-lg-2 px-0 formCard mb-5 text-center mx-auto">
+              <img
+                className="col-12 p-0"
+                src="img/form/question1/renegocier.png"
+                alt="renegocier"
+              />
+              <p className="my-auto py-2">Renégocier</p>
+            </div>
+            <div className="col-10 col-sm-5 col-lg-2 px-0 formCard mb-5 text-center mx-auto">
+              <img
+                className="col-12 p-0"
+                src="img/form/question1/autresprojets.jpg"
+                alt="autres projets"
+              />
+              <p className="my-auto py-2">Autres projets</p>
+            </div>
+          </div>
+        </div>
+      </form>
+    </StyledForm>
+  );
+}
 
-// export default Question1;
+Question1.propTypes = {
+  handleChange: PropTypes.string.isRequired,
+};
+
+export default Question1;
