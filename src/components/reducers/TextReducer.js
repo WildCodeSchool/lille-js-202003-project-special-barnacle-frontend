f1df5017fa8bc110ba37d44b19e2069014e06d92
yann <yanndubois4@gmail.com>
2020-07-08 16:40:11 +0200

    link redux operational

--- src/components/reducers/TextReducer.js
+++ src/components/reducers/TextReducer.js
@@ -1,13 +1,11 @@
 const TextReducer = (state = [], action) => {
   switch (action.type) {
     case 'ADDTEXT':
-      return {
-        ...state,
-        question1: action.text,
-      };
+      return { ...state, question1: action.text };
 
     default:
       return state;
   }
 };
+
 export default TextReducer;
