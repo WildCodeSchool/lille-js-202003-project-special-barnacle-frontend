0bf350ae7d27eb6fc20edaa1658f7aa5f65776d5
yann <yanndubois4@gmail.com>
2020-07-15 15:33:18 +0200

    reactor(conditinal-rendering): remove comments

--- src/components/form/Form.jsx
+++ src/components/form/Form.jsx
@@ -5,19 +5,6 @@ import Question1 from './question/1';
 
 import Question2 from './question/2';
 import Question3 from './question/3';
-// import Question4 from './question/4';
-// import Question4a from './question/4a';
-// import Question5 from './question/5';
-// import Question5a from './question/5a';
-// import Question5b from './question/5b';
-// import Question6 from './question/6';
-// import Question7 from './question/7';
-// import Question7a from './question/7a';
-// import Question7b from './question/7b';
-// import Question10 from './question/10';
-// import Question10a from './question/10a';
-// import Question10b from './question/10b';
-// import Question11 from './question/11';
 
 const Form = () => {
   const choice = useSelector((state) => state.bool.choice);
