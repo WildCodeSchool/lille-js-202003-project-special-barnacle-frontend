8d7374deb2eb7aa5ac460256af82988877a28033
Ghazi-1 <gh.maes@gmail.com>
2020-07-21 11:29:01 +0200

    add NosOffres page and link

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,8 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Apropos from './components/Apropos';
 import Header from './components/Header';
 import Homepage from './components/Homepage';
+import NosOffres from './components/NosOffres';
 import Footer from './components/Footer';
-import Question1 from './components/form/Question1';
-import Question2 from './components/form/Question2';
-import Question3 from './components/form/Question3';
-import Question3a from './components/form/Question3a';
 import Form from './components/form/Form';
 
 import './App.css';
@@ -24,24 +21,14 @@ function App() {
           <Route path="/about">
             <Apropos />
           </Route>
-          <Route path="/offer" component={() => <div>offer</div>} />
+          <Route path="/offer">
+            <NosOffres />
+          </Route>
           <Route path="/project" component={() => <div>Votre projet</div>} />
           <Route path="/contact" component={() => <div>contact</div>} />
           <Route path="/user/:id" component={() => <div>user</div>} />
           <Route path="/form">
             <Form />
-          </Route>
-          <Route exact path="/question1">
-            <Question1 />
-          </Route>
-          <Route exact path="/question2">
-            <Question2 />
-          </Route>
-          <Route exact path="/question3">
-            <Question3 />
-          </Route>
-          <Route exact path="/question3a">
-            <Question3a />
           </Route>
         </Switch>
         <Footer />
