e7c342b0c57016f6d9bfabfac347e787f7f3a4a1
berenice89 <berenice.despres@gmail.com>
2020-07-28 13:46:10 +0200

    solved some bugs

--- src/components/form/question/part-4/44.jsx
+++ src/components/form/question/part-4/44.jsx
@@ -1,48 +1,25 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import styled from 'styled-components';
+import React, { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+// import '../styleForm.css';
 
-const StyledForm = styled.section`
-  h1 {
-    color: #2a96af;
-  }
+const Question44 = () => {
+  const dispatch = useDispatch();
 
-  p {
-    color: #fff;
-    font-weight: 700;
-  }
+  const handleAnswer = (va) => {
+    dispatch({ type: 'ANSWER', question: 'question44', text: va });
+  };
 
-  .form {
-    background-color: #edf1f5;
-  }
+  useEffect(() => {
+    dispatch({ type: 'ANSWER' });
+  }, [dispatch]);
 
-  .formCard {
-    border-radius: 10px;
-    -webkit-box-shadow: 0px 0px 70px -26px rgba(0, 0, 0, 0.54);
-    -moz-box-shadow: 0px 0px 70px -26px rgba(0, 0, 0, 0.54);
-    box-shadow: 0px 0px 70px -26px rgba(0, 0, 0, 0.54);
-    transition: all 300ms;
-    background-color: #173753;
-  }
-
-  .formCard:hover {
-    transform: translate(0%, -1%);
-  }
-
-  input {
-    border-radius: 10px 10px 0 0;
-  }
-
-  input:focus {
-    border: 2px solid #173753;
-    background-color: #173753;
-  }
-`;
-
-function Question44({ handleChange, nextStep }) {
   return (
-    <StyledForm>
-      <form>
+    <div className="questionImg">
+      <div
+        onSubmit={(e) => {
+          e.preventDefault();
+        }}
+      >
         <div className="form container-fluid pb-5">
           <div className="row-cols-12 p-5 text-center">
             <h1>Vous êtes :</h1>
@@ -51,13 +28,13 @@ function Question44({ handleChange, nextStep }) {
             <div className="col-10 col-sm-5 col-lg-2 px-0 formCard mb-5 text-center mx-auto">
               <input
                 id="image"
-                value="Homme"
+                value="Un homme"
                 type="image"
-                alt="Un homme"
-                src="img/form/question44/homme.png"
+                alt="un homme"
+                src="img/form/question52/homme.png"
                 width="100%"
-                onClick={(e) => {
-                  handleChange(e, 'question44');
+                onClick={() => {
+                  handleAnswer('question52 - Un homme');
                 }}
               />
               <p className="my-auto py-2">Un homme</p>
@@ -66,13 +43,13 @@ function Question44({ handleChange, nextStep }) {
             <div className="col-10 col-sm-5 col-lg-2 px-0 formCard mb-5 text-center mx-auto">
               <input
                 id="image"
-                value="Femme"
+                value="Une femme"
                 type="image"
-                alt="Une femme"
-                src="img/form/question44/femme.png"
+                alt="une femme"
+                src="img/form/question52/femme.png"
                 width="100%"
-                onClick={(e) => {
-                  handleChange(e, 'question44');
+                onClick={() => {
+                  handleAnswer('question52 - Une femme');
                 }}
               />
               <p className="my-auto py-2">Une femme</p>
@@ -80,38 +57,22 @@ function Question44({ handleChange, nextStep }) {
             <div className="col-10 col-sm-5 col-lg-2 px-0 formCard mb-5 text-center mx-auto">
               <input
                 id="image"
-                value="Société"
+                value="Une societe"
                 type="image"
-                alt="Une société"
-                src="img/form/question44/societe.png"
+                alt="une societe"
+                src="img/form/question52/societe.jpg"
                 width="100%"
-                onClick={(e) => {
-                  handleChange(e, 'question44');
+                onClick={() => {
+                  handleAnswer('question52 - Une societe');
                 }}
               />
-
               <p className="my-auto py-2">Une société</p>
             </div>
           </div>
-          <div>
-            <button
-              type="button"
-              onClick={(e) => {
-                nextStep(e);
-              }}
-            >
-              NEXT
-            </button>
-          </div>
         </div>
-      </form>
-    </StyledForm>
+      </div>
+    </div>
   );
-}
-
-Question44.propTypes = {
-  handleChange: PropTypes.string.isRequired,
-  nextStep: PropTypes.number.isRequired,
 };
 
 export default Question44;
---
+++ src/components/form/question/part-4/52.jsx
@@ -0,0 +1,64 @@
+import React, { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+// import '../styleForm.css';
+
+const Question52 = () => {
+  const dispatch = useDispatch();
+
+  const handleAnswer = (va) => {
+    dispatch({ type: 'ANSWER', question: 'question52', text: va });
+  };
+
+  useEffect(() => {
+    dispatch({ type: 'ANSWER' });
+  }, [dispatch]);
+
+  return (
+    <div className="questionImg">
+      <div
+        onSubmit={(e) => {
+          e.preventDefault();
+        }}
+      >
+        <div className="form container-fluid pb-5">
+          <div className="row-cols-12 p-5 text-center">
+            <h1>Vous êtes :</h1>
+          </div>
+          <div className="row text-center">
+            <div className="col-10 col-sm-5 col-lg-2 px-0 formCard mb-5 text-center mx-auto">
+              <input
+                id="image"
+                value="Un homme"
+                type="image"
+                alt="un homme"
+                src="img/form/question52/homme.png"
+                width="100%"
+                onClick={() => {
+                  handleAnswer('question52 - Un homme');
+                }}
+              />
+              <p className="my-auto py-2">Un homme</p>
+            </div>
+
+            <div className="col-10 col-sm-5 col-lg-2 px-0 formCard mb-5 text-center mx-auto">
+              <input
+                id="image"
+                value="Une femme"
+                type="image"
+                alt="une femme"
+                src="img/form/question52/femme.png"
+                width="100%"
+                onClick={() => {
+                  handleAnswer('question52 - Une femme');
+                }}
+              />
+              <p className="my-auto py-2">Une femme</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Question52;
