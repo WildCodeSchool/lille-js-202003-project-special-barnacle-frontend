f1906fd5ef6f0b9971d3d50f39225444545ea803
Julien Martos <ju_sxm@hotmail.fr>
2020-07-28 14:02:12 +0200

    fix: remove margin top from each questions on part-2 and not necessary div

--- src/components/form/question/part-2/19a.jsx
+++ src/components/form/question/part-2/19a.jsx
@@ -21,28 +21,26 @@ function Question19a() {
   };
 
   return (
-    <div>
-      <div className="form container-fluid pb-5">
-        <div className="row-cols-12 py-4 p-lg-5 text-center my-3 mb-lg-5">
-          <h1>Quel est le montant de l’investissement?</h1>
-        </div>
-        <form
-          onSubmit={(e) => {
-            handleSubmit(e);
-          }}
-        >
-          <input
-            className="questionEnter col-10 col-sm-6 py-4 px-0 px-3 px-lg-5 mb-5 mx-auto d-flex justify-content-between"
-            type="text"
-            placeholder="Montant"
-            label="question19a"
-            value={answer}
-            onChange={(e) => {
-              handleChange(e.target.value);
-            }}
-          />
-        </form>
+    <div className="form container-fluid pb-5">
+      <div className="row-cols-12 py-4 p-lg-5 text-center mb-3 mb-lg-5">
+        <h1>Quel est le montant de l’investissement?</h1>
       </div>
+      <form
+        onSubmit={(e) => {
+          handleSubmit(e);
+        }}
+      >
+        <input
+          className="questionEnter col-10 col-sm-6 py-4 px-0 px-3 px-lg-5 mb-5 mx-auto d-flex justify-content-between"
+          type="number"
+          placeholder="Montant"
+          label="question19a"
+          value={answer}
+          onChange={(e) => {
+            handleChange(e.target.value);
+          }}
+        />
+      </form>
     </div>
   );
 }
--- src/components/form/question/part-2/32.jsx
+++ src/components/form/question/part-2/32.jsx
@@ -20,28 +20,26 @@ function Question32() {
     setAnswer(value);
   };
   return (
-    <div>
-      <div className="form container-fluid pb-5">
-        <div className="row-cols-12 py-4 p-lg-5 text-center my-3 mb-lg-5">
-          <h1>Confirmez le montant à racheter:</h1>
-        </div>
-        <form
-          onSubmit={(e) => {
-            handleSubmit(e);
-          }}
-        >
-          <input
-            className="questionEnter col-10 col-sm-6 py-4 px-0 px-3 px-lg-5 mb-5 mx-auto d-flex justify-content-between"
-            type="text"
-            placeholder="Montant"
-            label="question32"
-            value={answer}
-            onChange={(e) => {
-              handleChange(e.target.value);
-            }}
-          />
-        </form>
+    <div className="form container-fluid pb-5">
+      <div className="row-cols-12 py-4 p-lg-5 text-center mb-3 mb-lg-5">
+        <h1>Confirmez le montant à racheter:</h1>
       </div>
+      <form
+        onSubmit={(e) => {
+          handleSubmit(e);
+        }}
+      >
+        <input
+          className="questionEnter col-10 col-sm-6 py-4 px-0 px-3 px-lg-5 mb-5 mx-auto d-flex justify-content-between"
+          type="number"
+          placeholder="Montant"
+          label="question32"
+          value={answer}
+          onChange={(e) => {
+            handleChange(e.target.value);
+          }}
+        />
+      </form>
     </div>
   );
 }
