e3d0138b7aae6254199f516aa7bd97ee651f17be
berenice89 <berenice.despres@gmail.com>
2020-07-28 14:09:42 +0200

    deleted unused code

--- src/App.jsx
+++ src/App.jsx
@@ -4,20 +4,6 @@ import Apropos from './components/Apropos';
 import Header from './components/Header';
 import Homepage from './components/Homepage';
 import Footer from './components/Footer';
-import Question1 from './components/form/Question1';
-import Question2 from './components/form/Question2';
-import Question3 from './components/form/Question3';
-import Question3a from './components/form/Question3a';
-import Question44 from './components/form/question/part-4/44';
-import Question45 from './components/form/question/part-4/45';
-import Question46 from './components/form/question/part-4/46';
-import Question47 from './components/form/question/part-4/47';
-import Question47a from './components/form/question/part-4/47a';
-import Question48 from './components/form/question/part-4/48';
-import Question49 from './components/form/question/part-4/49';
-import Question50 from './components/form/question/part-4/50';
-import Question51 from './components/form/question/part-4/51';
-import Question52 from './components/form/question/part-4/52';
 import Form from './components/form/Form';
 
 import './App.css';
@@ -40,48 +26,6 @@ function App() {
           <Route path="/user/:id" component={() => <div>user</div>} />
           <Route path="/form">
             <Form />
-          </Route>
-          <Route exact path="/question1">
-            <Question1 />
-          </Route>
-          <Route exact path="/question2">
-            <Question2 />
-          </Route>
-          <Route exact path="/question3">
-            <Question3 />
-          </Route>
-          <Route exact path="/question3a">
-            <Question3a />
-          </Route>
-          <Route exact path="/question44">
-            <Question44 />
-          </Route>
-          <Route exact path="/question45">
-            <Question45 />
-          </Route>
-          <Route exact path="/question46">
-            <Question46 />
-          </Route>
-          <Route exact path="/question47">
-            <Question47 />
-          </Route>
-          <Route exact path="/question47a">
-            <Question47a />
-          </Route>
-          <Route exact path="/question48">
-            <Question48 />
-          </Route>
-          <Route exact path="/question49">
-            <Question49 />
-          </Route>
-          <Route exact path="/question50">
-            <Question50 />
-          </Route>
-          <Route exact path="/question51">
-            <Question51 />
-          </Route>
-          <Route exact path="/question52">
-            <Question52 />
           </Route>
         </Switch>
         <Footer />
