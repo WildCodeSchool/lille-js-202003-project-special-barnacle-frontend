bbf90f09cc355c29c738ca583afc159a4ceed90a
Julien Martos <ju_sxm@hotmail.fr>
2020-07-29 17:15:25 +0200

    fix: change route in DecouvrezNegodi-footer

--- src/components/footer-compo/DecouvrezNegodi.jsx
+++ src/components/footer-compo/DecouvrezNegodi.jsx
@@ -1,5 +1,5 @@
-import { Link } from 'react-router-dom';
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const DecouvrezNegodi = () => {
   return (
@@ -7,7 +7,7 @@ const DecouvrezNegodi = () => {
       <h4 className="list-group-item listBackground ">Découvrez Negodi</h4>
       <ul>
         <li className="list-group-item listBackground ">
-          <Link to="/offer">Nos offres</Link>
+          <Link to="offer">Nos offres</Link>
         </li>
         <li className=" list-group-item  listBackground ">
           <Link to="/form"> Démarrer mon projet</Link>
